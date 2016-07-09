32689eefac9be3f1873b9711afc77aa941800437
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-09 10:19:43 -0500

    Create test file for programming study cards.
    Add test for problem:
    Given a list of edges in an undirected graph, determine whether all
    points are connected.

---
+++ test_reps.py
@@ -0,0 +1,31 @@
+from is_connected import is_connected
+from nose.tools import assert_equals
+# Given a list of edges in an undirected graph, determine
+# whether all points are connected.
+def test_empty_list():
+    assert_equals(is_connected([]), True)
+
+def test_self_loop():
+    assert_equals(is_connected([[1, 1]]), True)
+
+def test_two_points():
+    assert_equals(is_connected([[1, 2]]), True)
+
+def test_chain_end_connected():
+    assert_equals(is_connected([[1, 2], [3, 4], [2, 4]]), True)
+
+def test_chain_front_connected():
+    assert_equals(is_connected([[1, 2], [3, 4], [1, 3]]), True)
+
+def test_chain_front_to_end_connected():
+    assert_equals(is_connected([[1, 2], [3, 4], [1, 4]]), True)
+
+def test_two_disjoint():
+    assert_equals(is_connected([[1, 2], [3, 4]]), False)
+
+def test_three_two_disjoint():
+    assert_equals(is_connected([[1, 2], [3, 4], [1, 6]]), False)
+
+def test_long_chain():
+    input_list = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+    assert_equals(is_connected(input_list), True)
