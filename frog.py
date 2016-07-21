c455befca53ab764fb7986335e1bd1e2de4ff431
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-20 20:15:09 -0500

    Add caching support for frog example.

--- frog.py
+++ frog.py
@@ -1,9 +1,15 @@
-def frog(steps, jumps):
+def frog(steps, jumps, cache=None):
     if steps < 0:
         return 0
     if steps == 0 or steps == 1:
         return 1
-    return sum((frog(steps-x, jumps) for x in xrange(1, jumps + 1)))
+    if cache is None:
+        cache = {}
+    elif steps in cache:
+        return cache[steps]
+    partial_result = sum(frog(steps-x, jumps, cache) for x in xrange(1, jumps + 1))
+    cache[steps] = partial_result
+    return partial_result
 
 
 
