042e6a77642cf0db92430e33f6938a717cd666d3
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-25 13:07:49 -0500

    Add has_cycle to dfs_graph.
    Add tests for has_cycle.

--- dfs_graph.py
+++ dfs_graph.py
@@ -23,3 +23,27 @@ def dfs_graph(edges, start, target):
         seen.update(new_edges)
     return False
 
+# If we have something that gives us the connected components,
+# it would help to use those components and iterate through them
+# looking for cycles.
+def has_cycle(edges):
+    # Since we don't want to have a single edge connecting two
+    # vertices to count as a cycle, we'll only add each edge once.
+    connections = defaultdict(list)
+    for edge in edges:
+        connections[edge[0]].append(edge[1])
+    for edge in edges:
+        # No need to look at the edge[1] values, because they'll
+        # all be checked when traversing the paired edge[0]
+        point = edge[0]
+        seen = set()
+        seen.add(point)
+        stack = connections[point]
+        while len(stack) > 0:
+            current = stack.pop()
+            if current in seen:
+                return True
+            seen.add(current)
+            stack.extend(connections[current])
+    return False
+    
--- test_dfs_graph.py
+++ test_dfs_graph.py
@@ -1,4 +1,7 @@
 from dfs_graph import dfs_graph
+from dfs_graph import has_cycle
+
+# dfs_graph_tests
 def test_empty_list():
     assert not dfs_graph([], 1, 'A')
 
@@ -35,3 +38,25 @@ def test_forked():
 def test_long_chain():
     input_list = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
     assert dfs_graph(input_list, 1, 6)
+
+# has_cycle tests
+def test_has_cycle_empty():
+    assert not has_cycle([])
+
+def test_has_cycle_single_edge():
+    assert not has_cycle([[1, 2]])
+
+def test_has_cycle_self_loop():
+    assert has_cycle([[1, 1]])
+
+def test_has_cycle_two_points():
+    assert has_cycle([[1, 2], [2, 1]])
+
+def test_has_cycle_three_points():
+    assert has_cycle([[1, 2], [2, 3], [3, 1]])
+
+def test_has_cycle_not_beginning():
+    assert has_cycle([[1, 2], [2, 3], [3, 2]])
+
+
+
