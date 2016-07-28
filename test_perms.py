9a0f18d29f21f840e1d8c8a64c055b2ad76a8800
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-28 11:00:06 -0500

    Add tests for the new versions of perms_recurse.

--- test_perms.py
+++ test_perms.py
@@ -1,6 +1,9 @@
 from perms_recurse import perms
 from perms_recurse import perms_partial
+from perms_recurse import perms_partial2
+from perms_recurse import perms
 from nose.tools import assert_equals
+from itertools import permutations
 
 def test_perms_empty():
     assert_equals(perms(""), set())
@@ -27,10 +30,37 @@ def test_perms_partial_two():
     assert_equals(perms_partial("ab"), ["ab", "ba"])
 
 def test_perms_partial_three():
-    # Use sets to avoid mismatches due to ordering
-    expected = set(["abc", "acb", "bac", "bca", "cab", "cba"])
-    assert_equals(set(perms_partial("abc")), expected)
+    # Sort to avoid mismatches due to ordering
+    expected = sorted(["abc", "acb", "bac", "bca", "cab", "cba"])
+    assert_equals(sorted(perms_partial("abc")), expected)
+
+def test_perms_partial_four():
+    expected = ["".join(x) for x in permutations("6789")]
+    assert_equals(sorted(perms_partial("6789")), expected)
 
 # This non-set version should have multiple entries for each duplicate
-def test_perms_partial_duplicates():
-    assert_equals(perms_partial("bb"), ["bb", "bb"])
+def test_perms_partial2_duplicates():
+    assert_equals(perms_partial2("bb"), ["bb", "bb"])
+
+# Alternate (more standard) version
+def test_perms_partial2_empty():
+    assert_equals(perms_partial2(""), [""])
+
+def test_perms_partial2_single():
+    assert_equals(perms_partial2("a"), ["a"])
+
+def test_perms_partial2_two():
+    assert_equals(sorted(perms_partial2("ab")), ["ab", "ba"])
+
+def test_perms_partial2_three():
+    # Sort to avoid mismatches due to ordering
+    expected = sorted(["abc", "acb", "bac", "bca", "cab", "cba"])
+    assert_equals(sorted(perms_partial2("abc")), expected)
+
+def test_perms_partial2_four():
+    expected = ["".join(x) for x in permutations("6789")]
+    assert_equals(sorted(perms_partial2("6789")), expected)
+
+# This non-set version should have multiple entries for each duplicate
+def test_perms_partial2_duplicates():
+    assert_equals(perms_partial2("bb"), ["bb", "bb"])
