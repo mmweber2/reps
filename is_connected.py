0683fb137fc33040f5296ba3ffbeecd815ed1253
Mary Ellen Weber <mmweber2@gmail.com>
2017-08-22 17:29:12 -0500

    Fix error in is_connected.py.
    Add documentation.

--- is_connected.py
+++ is_connected.py
@@ -1,6 +1,17 @@
 from collections import defaultdict
 
 def is_connected(edges):
+    """Returns True if the edges represent one connected chunk.
+
+    Args:
+        edges: list of length-two lists or tuples.
+            For example, [1, 2] indicates an edge from vertex "1" to vertex "2".
+            Vertices must be hashable.
+
+    Returns:
+        True if the edges are a single connected chunk, False otherwise.
+        Returns True if edges is empty.
+    """
     if not edges:
         return True # or False, depending on specification
     graph = defaultdict(list)
@@ -13,5 +24,6 @@ def is_connected(edges):
     while queue:
         current = queue.pop(0)
         queue.extend(x for x in graph[current] if x not in seen)
+        seen.add(current) # Optional
         seen.update(graph[current])
-    return len(seen) == len(connected_to)
+    return len(seen) == len(graph)
