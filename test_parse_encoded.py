eb5ac56a3e17177d1b9d3b00f7103914c6f37eeb
Mary Ellen Weber <mmweber2@gmail.com>
2017-08-24 16:53:39 -0500

    Add 4 more tests.
    Add test with parens for a single-digit letter.
    Add test with parens for a double-digit letter.
    Add test with parens for two single-digit letters.
    Add test combinining single-digit letters, double-digit letters, and parentheses.

--- test_parse_encoded.py
+++ test_parse_encoded.py
@@ -25,3 +25,27 @@ def test_two_digit_multiple_occurrence():
     arr[0] = 1
     arr[25] = 2
     assert_equals(parse_encoded("26#126#"), arr)
+
+def test_multiple_count_single_digit():
+    arr = get_base_array()
+    arr[0] = 2
+    assert_equals(parse_encoded("1(2)"), arr)
+
+def test_multiple_count_double_digit():
+    arr = get_base_array()
+    arr[12] = 2
+    assert_equals(parse_encoded("13#(2)"), arr)
+
+def test_multiple_parens():
+    arr = get_base_array()
+    arr[6] = 5
+    arr[2] = 2
+    assert_equals(parse_encoded("3(2)7(5)"), arr)
+
+def test_complex():
+    arr = get_base_array()
+    arr[0] = 1
+    arr[1] = 1
+    arr[25] = 1
+    arr[23] = 3
+    assert_equals(parse_encoded("1226#24#(3)"), arr)
