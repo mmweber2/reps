e0da56acd6c4c31cdd980abdc9c17aa1f1287639
Mary Ellen Weber <mmweber2@gmail.com>
2017-08-24 16:49:52 -0500

    Fix a positioning issue with opening parens.

--- parse_encoded.py
+++ parse_encoded.py
@@ -1,18 +1,18 @@
-'''
-Based on a problem from HackerRank.
-
-Input: An encoded string where 1 = a, 2 = b, and so on.
-  For two-digit numbers (beginning with j = 10), a # follows the number,
-  so j appears as 10# in the string.
-  If a letter appears multiple times in a row, it is followed by (count)
-  in parentheses, such as aaa = 1(3).
-
-  It is given that the input string will be correctly formatted.
-         
-Output: An array of length 26 where index 0 indicates the frequency of letter a as
-  an integer, index 1 for letter b, index 25 for letter z, etc.
-''' 
+# Based on a problem from HackerRank.
 def parse_encoded(encoded_string):
+    '''Returns a frequency list of the encoded string.
+
+        Input: An encoded string where 1 = a, 2 = b, and so on.
+        For two-digit numbers (beginning with j = 10), a # follows the number,
+        so j appears as 10# in the string.
+        If a letter appears multiple times in a row, it is followed by (count)
+        in parentheses, such as aaa = 1(3).
+
+        It is given that the input string will be correctly formatted.
+                
+        Output: An array of length 26 where index 0 indicates the frequency of letter a as
+        an integer, index 1 for letter b, index 25 for letter z, etc.
+    ''' 
     # There may not be any parens in s, but this will make them easier to manage
     sections = encoded_string.split(")")
     freqs = [0] * 26
@@ -27,15 +27,17 @@ def parse_encoded(encoded_string):
                 # The # sign is the end of this letter, so move there
                 pos += 2
             if pos + 1 < len(section) and section[pos + 1] == "(":
-                # pos to end of section should be the contents of the parentheses
+                # Move past the digit and the opening parenthesis
+                pos += 2
+                # pos to end of section should contain the contents of the parentheses
                 freqs[letter_id] += int(section[pos:])
                 # Since the section breaks on ")", this is the end of the section,
                 #    so break out of the while loop and avoid duplicate additions
                 #    of this letter
                 break
-            pos += 1
+            # Add any singly-occuring letters
             freqs[letter_id] += 1
+            pos += 1
     return freqs
 
-                
             
