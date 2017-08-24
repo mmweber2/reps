03839dd6b2bf495f803669a29a08abc7820185ad
Mary Ellen Weber <mmweber2@gmail.com>
2017-08-24 17:05:48 -0500

    Remove practice files from tracking.

--- jul7.py
+++
@@ -1,8 +0,0 @@
-def dfs(tree, target):
-    if tree is None:
-        return False
-    if tree.value == target:
-        return True
-    if tree.value < target:
-        return dfs(tree.left, target)
-    return dfs(tree.right, target)
