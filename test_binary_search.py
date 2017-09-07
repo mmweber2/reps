836043cae7e14967f322399fe8cbcdbc997d56e0
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-07 17:34:12 -0500

    Add 3 tests for binary_search.
    Add test: single item, target absent.
    Add test: two items, target present.
    Add test: two items, target absent.

--- test_binary_search.py
+++ test_binary_search.py
@@ -15,3 +15,18 @@ def test_single_item_absent():
     li = [0]
     assert_equals(bsearchi(li, 1), -1)
     assert_equals(bsearchr(li, 1), -1)
+
+def test_two_items_sorted_present():
+    li = [0, 5]
+    assert_equals(bsearchi(li, 0), 0)
+    assert_equals(bsearchr(li, 0), 0)
+
+def test_two_items_sorted_absent():
+    li = [0, 5]
+    assert_equals(bsearchi(li, 10), -1)
+    assert_equals(bsearchr(li, 10), -1)
+
+def test_three_items_sorted_present():
+    li = [0, 1, 5]
+    assert_equals(bsearchi(li, 1), 1)
+    assert_equals(bsearchr(li, 1), 1)
