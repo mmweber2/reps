b8263db9a6c74bee6204f929c4612ac841320b0d
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-07 17:29:32 -0500

    Write 3 tests for binary search.
    Add test: empty lists.
    Add test: single item, present.
    Add test: single item, absent.

---
+++ test_binary_search.py
@@ -0,0 +1,17 @@
+from binary_search import bsearchr
+from binary_search import bsearchi
+from nose.tools import assert_equals
+
+def test_empty_lists():
+    assert_equals(bsearchi([], 0), -1)
+    assert_equals(bsearchr([], 0), -1)
+
+def test_single_item_present():
+    li = [0]
+    assert_equals(bsearchi(li, 0), 0)
+    assert_equals(bsearchr(li, 0), 0)
+
+def test_single_item_absent():
+    li = [0]
+    assert_equals(bsearchi(li, 1), -1)
+    assert_equals(bsearchr(li, 1), -1)
