18a1d1c76b3358c9005cbab4e2c8377f847eb892
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-20 14:08:41 -0500

    Add 8 tests for superbalanced.py.
    Add tests for:
      Single leaf from root.
      Two leaves from root.
      Two leaves, difference of one, left side.
      Two leaves, difference of one, right side.
      Not balanced, but superbalanced (left children only).
      Not balanced, but superbalanced (right children only).
      Not superbalanced because heights differ by 2.
      Not superbalanced because there are 3 heights.

--- cake/test_superbalanced.py
+++ cake/test_superbalanced.py
@@ -4,10 +4,71 @@ from superbalanced import is_superbalanced as f
 
 # No tests included for the BinaryTreeNode class itself,
 #   as it came from the problem statement.
+
+# The values aren't being examined at all, so we should be able to
+#   test with the same values every time.
 def test_empty_tree():
     assert f([])
 
 def test_single_node():
-    a = BinaryTreeNode("1")
+    a = BinaryTreeNode(1)
     assert f(a)
+
+def test_single_leaf():
+    a = BinaryTreeNode(1)
+    a.insert_left(2)
+    assert f(a)
+
+def test_two_leaves_diff_zero():
+    a = BinaryTreeNode(1)
+    a.insert_left(2)
+    a.insert_right(2)
+    assert f(a)
+
+def test_two_leaves_diff_one_left():
+    a = BinaryTreeNode(1)
+    b = a.insert_left(2)
+    b.insert_left(2)
+    assert f(a)
+
+def test_two_leaves_diff_one_right():
+    a = BinaryTreeNode(1)
+    b = a.insert_left(2)
+    b.insert_right(2)
+    assert f(a)
+
+def test_left_only():
+    # This tree is not balanced in the normal sense, but it meets
+    #   the definition of superbalanced.
+    a = BinaryTreeNode(1)
+    b = a.insert_left(2)
+    b.insert_left(2)
+    assert f(a)
+
+def test_right_only():
+    a = BinaryTreeNode(1)
+    b = a.insert_right(2)
+    b.insert_right(2)
+    assert f(a)
+
+def test_unbalanced_two_heights():
+    a = BinaryTreeNode(1)
+    # Two children in root's right subtree
+    a.insert_right(2).insert_left(3)
+    # Root's left subtree
+    b = a.insert_left(2)
+    b.insert_left(3)
+    d = b.insert_right(4)
+    d.insert_left(5).insert_left(6)
+    assert not f(a)
+
+def test_unbalanced_3_heights():
+    a = BinaryTreeNode(1)
+    a.insert_right(2)
+    b = a.insert_left(2)
+    b.insert_left(3).insert_left(4)
+    b.insert_right(4)
+    assert not f(a)
+
+
 
