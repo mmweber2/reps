a3f48eb7fd0242bd1a44dafd85f8b1b1a0783080
Mary Ellen Weber <mmweber2@gmail.com>
2017-10-17 20:13:58 -0500

    Change xrange to tuple of four numbers.

--- primes100.py
+++ primes100.py
@@ -4,10 +4,8 @@ def print_primes():
     primes = range(98) # 100, 99, and 98 cannot be prime
     # primes[0] is already 0, so just mark 1 as not prime
     primes[1] = 0
-    # Largest prime less than sqrt(10) is 7
-    for factor in xrange(2, 8):
-        if not primes[factor]:
-            continue
+    # Primes less than 10
+    for factor in (2, 3, 5, 7):
         for product in xrange(factor * 2, 98, factor):
             primes[product] = None
     for prime in primes:
