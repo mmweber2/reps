39b3f898202137b33900899d73934813f6d5ec2d
Mary Ellen Weber <mmweber2@gmail.com>
2017-12-01 12:17:55 +0900

    Add editorial solution for Wire Removal.

--- hackerrank/wire-removal.py
+++ hackerrank/wire-removal.py
@@ -1,26 +1,32 @@
 # HackerRank problem Wire Removal
 # https://www.hackerrank.com/contests/hourrank-24/challenges/wire-removal/problem 
 
-# Note: This solution does not currently pass the test cases.
-#   Some of the nodes are discovered before their parents, causing KeyErrors.
+# Solution modified from C++ editorial. In progress.
 from collections import defaultdict
 
-if __name__ == "__main__":
-    n = int(raw_input().strip())
-    tree = defaultdict(list)
-    edges = []
-    depths = {1:0}
-    for _ in xrange(n-1):
-        x, y = map(int, raw_input().strip().split(' '))
-        edges.append((x, y))
-        tree[x].append(y)
-        depths[y] = depths[x] + 1
-    total_expected = 0.0
-    total_depth = float(sum(depths.values()))
-    subtree_sizes = {}
-    for node in sorted(depths, key=lambda x: depths[x], reverse=True):
-        subtree_size = 1 + sum(subtree_sizes[x] for x in tree[node])
-        subtree_sizes[node] = subtree_size
-        edge_prob = depths[node] / total_depth
-        total_expected += edge_prob * (n - subtree_size)
-    print total_expected
+def dfs(node, depth, n, tree, subtree_sizes, visited):
+    global total_expected
+    global total_depth
+    visited[node] = True
+    subtree_sizes[node] = 1
+    for child in tree[node]:
+        if not visited[child]:
+            dfs(child, depth + 1, n, tree, subtree_sizes, visited)
+            subtree_sizes[node] += subtree_sizes[child]
+    if node != 1:
+        total_expected += depth * (n - subtree_sizes[node])
+        total_depth += depth
+        
+n = int(raw_input())
+subtree_sizes = defaultdict(int)
+total_expected = 0
+total_depth = 0
+visited = [False] * (n + 1)
+
+tree = defaultdict(list)
+for i in xrange(n - 1):
+    u, v = map(int, raw_input().strip().split())
+    tree[u].append(v)
+    tree[v].append(u)
+dfs(1, 0, n, tree, subtree_sizes, visited)
+print total_expected / float(total_depth)
